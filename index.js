fae83f96110170695075f8bfe459c93b6257b3d3
chandev0908 <chandev0908@gmail.com>
2021-02-17 23:39:40 +0800

    edited embed message

--- index.js
+++ index.js
@@ -13,50 +13,82 @@ client.distube = new DisTube(client, {
 });
 client.distube
   .on("playSong", (message, queue, song) => {
+    let mode = client.distube.toggleAutoplay(message);
     const playSongEmbed = new discord.MessageEmbed()
       .setColor("#0099ff")
+      .setTitle(`Now Playing!`)
       .setDescription(
-        `Playing ${song.name}\nDuration ${song.formattedDuration}\nRequested by: ${song.user}`
-      );
+        `Playing ${song.name}\nDuration: **${song.formattedDuration}**\nRequested by: ${song.user}\nAutoplay ${mode ? "❌" : "✅"}`
+      )
+      .setThumbnail(song.thumbnail)
     message.channel.send(playSongEmbed).then(async function (message) {
-      await message.react("🛑");
+      await message.react("⏹");
+      await message.react("⏯️")
       await message.react("⏭️");
 
       const filter = (reaction, user) => {
-        return (
-          ["🛑", "⏭️"].includes(reaction.emoji.name) && user.id === user.id
-        );
+        return ["⏹", "⏭️", "⏯️"].includes(reaction.emoji.name) && user.id === user.id;
       };
-      const collector = message.createReactionCollector(filter, {time:180000});
+      const collector = message.createReactionCollector(filter, {
+        time: song.duration > 0 ? song.duration * 1000 : 600000,
+      });
 
       collector.on("collect", (reaction, user) => {
         async function collect() {
-          let queue = await client.distube.getQueue(message);
-          if (!user.bot) {
-            if (reaction.emoji.name === "🛑") {
-              if(queue){
-                client.distube.stop(message);
-                message.reactions.removeAll()
-              }else{
-                return
+          if (!queue) return;
+          const member = message.guild.member(user);
+          if (member.voice.connection &&member.voice.connection !== member.guild.me.voice.connection)
+            return;
+          let isPaused = await client.distube.isPaused(message);
+          switch (reaction.emoji.name) {
+            case "⏹":
+              client.distube.stop(message);
+              message.reactions.removeAll();
+              break;
+            case "⏭️":
+              client.distube.skip(message);
+              message.reactions.removeAll();
+              break;
+            case "⏯️":
+              if(isPaused){
+                client.distube.resume(message, queue);
+              }else if(!isPaused){
+                client.distube.pause(message, queue);
               }
-            } else if (reaction.emoji.name === "⏭️") {
-              if(queue){
-                client.distube.skip(message);
-                message.reactions.removeAll()
-              }else{
-                return
-              }
-            }
-            reaction.users.remove(user.id);
           }
+          reaction.users.remove(user.id);
+          // let queue = await client.distube.getQueue(message);
+          // if (!user.bot) {
+          //   if (reaction.emoji.name === "🛑") {
+          //     if (queue) {
+          //       client.distube.stop(message);
+          //       message.reactions.removeAll();
+          //     } else {
+          //       return;
+          //     }
+          //   } else if (reaction.emoji.name === "⏭️") {
+          //     if (queue) {
+          //       client.distube.skip(message);
+          //       message.reactions.removeAll();
+          //     } else {
+          //       return;
+          //     }
+          //   } else if (reaction.emoji.name === "⏯️") {
+          //     if (queue) {
+
+          //     } else {
+          //       return;
+          //     }
+          //   }
+          //   reaction.users.remove(user.id);
+          // }
         }
         collect();
       });
       collector.on("end", (collectod, reason) => {
         async function collect() {
-          if(reason === 'time'){
-            message.reactions.removeAll()
+          if (reason === "time") {
+            message.reactions.removeAll();
           }
         }
         collect();
@@ -71,6 +103,14 @@ client.distube
       );
     message.channel.send(addSongEmbed);
   })
+  .on("playList", (message, queue, playlist, song) => {
+    const addPlaylistEmbed = new discord.MessageEmbed()
+      .setColor("#0099ff")
+      .setDescription(
+        `Play **${playlist.name}** \nThe playlist length is **${playlist.songs.length}**\nRequested by: ${song.user}\nNow playing **${song.name}** - **${song.formattedDuration}**`
+      );
+    message.channel.send(addPlaylistEmbed);
+  })
   .on("empty", (message) =>
     message.channel.send("Channel is empty. Leaving the channel")
   );
@@ -82,4 +122,4 @@ client.aliases = new discord.Collection();
 
 loadCommands(client);
 
-client.login(process.env.TOKEN);
+client.login("ODA1ODAzMDU3OTY2NDgxNDY4.YBgMlA.pjvSe9b2oFxN293dhuRzZoFcwaw");
