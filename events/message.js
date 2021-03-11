d4691cbe6d2e58d61f8ac8f22b06ba6cc092ee98
chandev0908 <chandev0908@gmail.com>
2021-03-11 23:40:31 +0800

    fix bugs

--- events/message.js
+++ events/message.js
@@ -1,4 +1,4 @@
-const ms = require('ms');
+const Duration = require('humanize-duration');
 module.exports = async (bot, message) => {
 	if (message.author.bot) return;
 
@@ -13,20 +13,17 @@ module.exports = async (bot, message) => {
 
 	if (!message.content.startsWith(prefix)) return;
 	const commandfile = bot.commands.get(cmd.slice(prefix.length).toString().toLowerCase()) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length).toString().toLowerCase()));;
-	console.log(commandfile.config.name)
-	console.log(commandfile)
-
-	if(bot.cooldown.has(`${message.author.id}-${commandfile.config.name}`)){
-		return message.channel.send(`You can use this command in ${ms(bot.cooldown.get(`${message.author.id}-${commandfile.config.name}`) - Date.now(), {long: true})}`);
-	}
+	// if(bot.cooldown.has(`${message.author.id}-${commandfile.config.name}`)){
+	// 	return message.channel.send(`You can use this command in ${ms(bot.cooldown.get(`${message.author.id}-${commandfile.config.name}`) - Date.now(), {long: true})}`);
+	// }
 	if (commandfile) {
 		commandfile.run(bot, message, args);
-		if(commandfile.config.cooldown) {
-			bot.cooldown.set(`${message.author.id}-${commandfile.config.name}`, Date.now() + commandfile.config.cooldown)
-			setTimeout(() => {
-				bot.cooldown.delete(`${message.author.id}-${commandfile.config.cooldown}`)
-			}, commandfile.config.cooldown)
-		}
+		// if(commandfile.config.cooldown) {
+		// 	bot.cooldown.set(`${message.author.id}-${commandfile.config.name}`, Date.now() + commandfile.config.cooldown)
+		// 	setTimeout(() => {
+		// 		bot.cooldown.delete(`${message.author.id}-${commandfile.config.cooldown}`)
+		// 	}, commandfile.config.cooldown)
+		// }
 	}
 
 }
